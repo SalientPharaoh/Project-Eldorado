9e475170127329f5b12f935bcd2f8598a5973dc7
Sahil Khirwal <sahilkhirwal1012@gmail.com>
2024-08-01 22:29:25 +0530

    fixing error

--- app/page.tsx
+++ app/page.tsx
@@ -96,7 +96,6 @@ const Hero = () => {
           <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 max-w-7xl mx-auto w-full px-2">
             <WobbleCard
               containerClassName="col-span-1 lg:col-span-2 h-full bg-pink-800 min-h-[500px] lg:min-h-[300px]"
-              className=""
             >
               <div className="max-w-xs">
                 <h2 className="text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
@@ -128,7 +127,7 @@ const Hero = () => {
                   Signup for Investing in Tomorrow, Today!
                 </h2>
                 <p className="mt-4 max-w-[26rem] text-left  text-base/6 text-neutral-200">
-                  Whether you're new to investing or a seasoned pro, our educational resources will help you navigate the world of startup investments. From understanding market trends to learning how to evaluate startups, we provide the tools you need to invest with confidence.
+                  Whether you are new to investing or a seasoned pro, our educational resources will help you navigate the world of startup investments. From understanding market trends to learning how to evaluate startups, we provide the tools you need to invest with confidence.
                 </p>
               </div>
               <Image
